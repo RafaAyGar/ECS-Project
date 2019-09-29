4368d026130f607f69cc7f4886b20c80abd5ae4c
Rafael Ayllon <rafaelaylln@gmail.com>
2019-09-29 22:40:21 +0200

    :8ball:

---
+++ ECS Project/Assets/Scripts/ECS_Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Collections;
+
+public class ECS_Manager : MonoBehaviour
+{
+    [SerializeField] private Transform t;
+
+    private EntityManager entityManager;
+   
+
+    private void Start()
+    {
+        entityManager = World.Active.EntityManager;
+        Entity entity = entityManager.CreateEntity(
+            typeof(Translation)
+        );
+
+        entityManager.SetComponentData(entity, new Translation { Value = t.position});
+    }
+
+    private void Update()
+    {
+
+    }
+}
